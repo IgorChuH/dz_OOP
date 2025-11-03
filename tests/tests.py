1527230d0a6a3f35e99738c612ae28fdefeca93b
Чухутин Игорь <chukhutinigor@yandex.ru>
2025-11-03 14:24:25 +0300

    логирование

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,14 @@
 import pytest
 
-from main import Product, Category
+from src.main import Product, Category
+
 
 @pytest.fixture()
 def product():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
 
 def test_init(product):
     assert product.name == "Samsung Galaxy S23 Ultra"
@@ -12,23 +16,29 @@ def test_init(product):
     assert product.price == 180000.0
     assert product.quality == 5
 
+
 @pytest.fixture
 def category():
-    return Category("Electronics",
-                         "Various electronic items",
-                         ["TV", "Radio", "Laptop"])
+    return Category(
+        "Electronics", "Various electronic items", ["TV", "Radio", "Laptop"]
+    )
+
 
 def test_category_init(category):
     assert category.name == "Electronics"
     assert category.description == "Various electronic items"
     assert category.products == ["TV", "Radio", "Laptop"]
 
+
 def test_class_counters():
     Category.category_count = 0
     Category.product_count = 0  # обязательно сбросить
-    category = Category("Electronics", "Various electronic items", ["TV", "Radio", "Laptop"])
+    category = Category(
+        "Electronics", "Various electronic items", ["TV", "Radio", "Laptop"]
+    )
     assert Category.category_count == 1
     assert Category.product_count == 3
+
 
 def test_multiple_categories():
     # Проверка счетчиков при создании нескольких объектов
@@ -40,4 +50,3 @@ def test_multiple_categories():
 
     assert Category.category_count == initial_category_count + 2
     assert Category.product_count == initial_product_count + 3
-
